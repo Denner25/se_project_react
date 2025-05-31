0b80df1ec76f61cf7d64a559618ac415feb0e77a
Denner Cardoso <denner_ttpc@hotmail.com>
2025-05-30 19:01:53 -0700

    begin work on header, main and modalwithform components

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,28 @@
+import WeatherCard from "../WeatherCard/WeatherCard";
+import { defaultClothingItems } from "../../utils/constants";
+import ItemCard from "../ItemCard/ItemCard";
+import "./Main.css";
+
+function Main({ weatherData }) {
+  return (
+    <main>
+      <WeatherCard />
+      <section className="cards">
+        <p className="cards__text">Today is 75° F / You may want to wear:</p>
+        <ul className="cards__list">
+          {defaultClothingItems
+            // .filter((item) => {
+            //   return item.weather === weatherData.type;
+            // })
+            .map((item) => {
+              // use map because it iterates through array and returns array
+              // key remains here (outermost piece of rendered element)
+              return <ItemCard key={item.id} item={item} />;
+            })}
+        </ul>
+      </section>
+    </main>
+  );
+}
+
+export default Main;
