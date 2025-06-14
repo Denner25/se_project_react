8eca53a9e2c54490a81efdbeef0c5077933a7460
Denner Cardoso <denner_ttpc@hotmail.com>
2025-06-13 21:25:39 -0700

    style profile component

--- src/components/ClothesSection/ClothesSection.jsx
+++ src/components/ClothesSection/ClothesSection.jsx
@@ -1,18 +1,19 @@
 import ItemCard from "../ItemCard/ItemCard";
+import "./ClothesSection.css";
 
 function ClothesSection({ clothingItems, onCardClick }) {
   return (
-    <div className="clothes-section__items">
-      <div>
-        <p>Your items</p>
-        <button>+ Add new</button>
-        <ul className="cards__list">
-          {clothingItems.map((item) => (
-            <ItemCard key={item._id} item={item} onCardClick={onCardClick} />
-          ))}
-        </ul>
+    <>
+      <div className="clothes-section__container">
+        <p className="clothes-section__text">Your items</p>
+        <button className="clothes-section__add-btn">+ Add new</button>
       </div>
-    </div>
+      <ul className="cards__list">
+        {clothingItems.map((item) => (
+          <ItemCard key={item._id} item={item} onCardClick={onCardClick} />
+        ))}
+      </ul>
+    </>
   );
 }
 
