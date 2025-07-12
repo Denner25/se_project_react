066b955b7680a9ccf22e7ed212797e115925387c
Denner Cardoso <denner_ttpc@hotmail.com>
2025-07-11 22:48:08 -0700

    fix basename error with env var

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter
-      basename="/"
+      basename={import.meta.env.PROD ? "/se_project_react" : undefined}
       future={{ v7_startTransition: true, v7_relativeSplatPath: true }}
     >
       <App />
