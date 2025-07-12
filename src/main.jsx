43c80fa6382e7e3143d427ef6726674c1dcb61e8
Denner Cardoso <denner_ttpc@hotmail.com>
2025-07-11 22:38:42 -0700

    fix basename error

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter
-      basename="/se_project_react"
+      basename="/"
       future={{ v7_startTransition: true, v7_relativeSplatPath: true }}
     >
       <App />
